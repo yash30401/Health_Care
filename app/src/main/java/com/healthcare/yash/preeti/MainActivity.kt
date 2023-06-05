a6d05c886ec9ca48e3a681e271f43047ce67f8d7
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-05 20:52:01 +0530

    Initial commit

---
+++ app/src/main/java/com/healthcare/yash/preeti/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.healthcare.yash.preeti
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.healthcare.yash.preeti.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding:ActivityMainBinding? =null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
