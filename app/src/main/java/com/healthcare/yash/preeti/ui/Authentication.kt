8fbce8c2a9946d5bd9076b256a0aed30e3fc491d
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-07 18:11:58 +0530

    lay-  Auth Layout partially completed

--- app/src/main/java/com/healthcare/yash/preeti/ui/Authentication.kt
+++ app/src/main/java/com/healthcare/yash/preeti/ui/Authentication.kt
@@ -3,10 +3,24 @@ package com.healthcare.yash.preeti.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healthcare.yash.preeti.R
+import com.healthcare.yash.preeti.databinding.ActivityAuthenticationBinding
 
 class Authentication : AppCompatActivity() {
+
+    private var _binding: ActivityAuthenticationBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        _binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
