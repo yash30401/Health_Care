42c5b67b3f4123dafae5852a2b67b519ddacd45a
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-09 16:42:53 +0530

    docs - Repository and FiebaseUtil completed

--- app/src/main/java/com/healthcare/yash/preeti/repositories/AuthRepository.kt
+++ app/src/main/java/com/healthcare/yash/preeti/repositories/AuthRepository.kt
@@ -2,28 +2,28 @@ package com.healthcare.yash.preeti.repositories
 
 import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthCredential
+import com.healthcare.yash.preeti.networking.NetworkResult
+import com.healthcare.yash.preeti.utils.await
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(
-    @ApplicationContext private val context: Context,
     private val firebaseAuth: FirebaseAuth,
 ) {
 
-    init {
 
+    suspend fun signinWithPhoneNo(credential: PhoneAuthCredential): NetworkResult<FirebaseUser> {
+        return try {
+            val result = firebaseAuth.signInWithCredential(credential).await()
+            NetworkResult.Success(result.user!!)
+        }catch (e:Exception){
+            NetworkResult.Error(e.message)
+        }
     }
 
-//    suspend fun signinWithPhoneNo(): NetworkResult<FirebaseUser> {
-//
-//    }
-
-//    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-//
-//    }
-
-
     fun logout() {
-
+        firebaseAuth.signOut()
     }
 }
--- app/src/main/java/com/healthcare/yash/preeti/utils/FirebaseUtils.kt
+++ app/src/main/java/com/healthcare/yash/preeti/utils/FirebaseUtils.kt
@@ -1,7 +1,17 @@
 package com.healthcare.yash.preeti.utils
 
 import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resumeWithException
 
 suspend fun <T> Task<T>.await():T{
-
+    return suspendCancellableCoroutine { cont->
+        addOnCompleteListener {
+            if(it.exception !=null){
+                cont.resumeWithException(it.exception!!)
+            }else{
+                cont.resume(it.result, null)
+            }
+        }
+    }
 }
