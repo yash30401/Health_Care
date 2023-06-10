7415929b7ecfa4c57bb6fa4c317aecd8ba4cf89e
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-10 16:34:58 +0530

    1. Drawer Layout Added And Problem Solved 2. Navigation Problem Solved 3. Logout Working 4. Exit Confirmation dialog added

---
+++ app/src/main/java/com/healthcare/yash/preeti/ui/fragments/MainFragment.kt
@@ -0,0 +1,105 @@
+package com.healthcare.yash.preeti.ui.fragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.healthcare.yash.preeti.R
+import com.healthcare.yash.preeti.databinding.FragmentMainBinding
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.healthcare.yash.preeti.viewmodels.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private var _binding:FragmentMainBinding?=null
+    private val binding get() = _binding!!
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
+    private val viewModel by viewModels<AuthViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+        val toolbar: Toolbar = rootView.findViewById(R.id.toolbar)
+        val activity = activity as AppCompatActivity
+        activity.setSupportActionBar(toolbar)
+        activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        activity.supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        drawerLayout = rootView.findViewById(R.id.drawerLayout)
+        navigationView = rootView.findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            activity, drawerLayout, toolbar,0,R.string.app_name
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle navigation item clicks here
+            when (menuItem.itemId) {
+                R.id.home -> {
+
+                    true
+                }
+                R.id.logout -> {
+                    viewModel.logout()
+                    findNavController().navigate(R.id.action_mainFragment_to_authentication2)
+                    true
+                }
+                // Add more navigation items and their handling
+                else -> false
+            }
+        }
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
