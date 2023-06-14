1fee82f150a37f275858cc2fa5041287fe3416c5
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-14 20:41:39 +0530

    func - Timer Implemented

--- app/src/main/java/com/healthcare/yash/preeti/other/Constants.kt
+++ app/src/main/java/com/healthcare/yash/preeti/other/Constants.kt
@@ -3,4 +3,7 @@ package com.healthcare.yash.preeti.other
 object Constants {
     const val TAG = "HEALTHCARETAGEFORLOGGING"
     const val AUTHVERIFICATIONTAG = "AUTHVERIFICATION"
+
+    //Coundown Time
+    const val COUNTDOWNTIMEINMINUTE = 1
 }
