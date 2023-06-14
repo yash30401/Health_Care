dfa199d92379a2b8cc746adccc620fe586de6379
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-14 20:42:07 +0530

    func - Timer Implemented

--- app/src/main/java/com/healthcare/yash/preeti/ui/fragments/OtpFragment.kt
+++ app/src/main/java/com/healthcare/yash/preeti/ui/fragments/OtpFragment.kt
@@ -1,6 +1,8 @@
 package com.healthcare.yash.preeti.ui.fragments
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
@@ -9,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -24,6 +27,7 @@ import com.google.firebase.auth.PhoneAuthProvider
 import com.healthcare.yash.preeti.R
 import com.healthcare.yash.preeti.databinding.FragmentOtpBinding
 import com.healthcare.yash.preeti.networking.NetworkResult
+import com.healthcare.yash.preeti.other.Constants.COUNTDOWNTIMEINMINUTE
 import com.healthcare.yash.preeti.other.Constants.TAG
 import com.healthcare.yash.preeti.viewmodels.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -45,12 +49,19 @@ class OtpFragment : Fragment(R.layout.fragment_otp) {
 
     private val viewModel by viewModels<AuthViewModel>()
 
+    private lateinit var countDownTimer: CountDownTimer
+    var isTimerRunning: Boolean? = false
+    var currentCounterTimeInMilliSeconds = 0L
+
+    private lateinit var phoneNumber: String
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentOtpBinding.bind(view)
 
         onClicks()
         setupPhoneNumberTextView()
+        startOtpResendTimer()
 
         binding.tvPhoneNo.setOnClickListener {
             editPhoneNumberAndNavigateBackToAuthScreen()
@@ -58,6 +69,10 @@ class OtpFragment : Fragment(R.layout.fragment_otp) {
 
         binding.ivEditPhoneNo.setOnClickListener {
             editPhoneNumberAndNavigateBackToAuthScreen()
+        }
+
+        binding.tvResend.setOnClickListener {
+            resendOtpToPhoneNumber()
         }
     }
 
@@ -81,7 +96,7 @@ class OtpFragment : Fragment(R.layout.fragment_otp) {
     }
 
     private fun setupPhoneNumberTextView() {
-        val phoneNumber = args.phoneNumber
+        phoneNumber  = args.phoneNumber
         val hiddenPhoneNumberText =
             "+91${phoneNumber.get(3)}${phoneNumber.get(4)}******${phoneNumber.get(11)}${
                 phoneNumber.get(12)
@@ -93,6 +108,49 @@ class OtpFragment : Fragment(R.layout.fragment_otp) {
     private fun editPhoneNumberAndNavigateBackToAuthScreen() {
         findNavController().navigate(R.id.action_otpFragment_to_authentication2)
     }
+
+    private fun startOtpResendTimer() {
+        currentCounterTimeInMilliSeconds = COUNTDOWNTIMEINMINUTE.toLong() * 60000L
+        countDownTimer = object : CountDownTimer(currentCounterTimeInMilliSeconds, 1000) {
+            override fun onTick(p0: Long) {
+                currentCounterTimeInMilliSeconds = p0
+                updateTimerUi()
+            }
+
+            override fun onFinish() {
+                binding.tvTimer.visibility = View.GONE
+
+                binding.tvResend.text = "Resend OTP"
+                binding.tvResend.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.blueResendTextColor
+                    )
+                )
+                isTimerRunning = false
+            }
+        }
+        countDownTimer.start()
+
+        isTimerRunning = true
+    }
+
+    private fun updateTimerUi() {
+        val minute = (currentCounterTimeInMilliSeconds / 1000) / 60
+        val seconds = (currentCounterTimeInMilliSeconds / 1000) % 60
+
+        binding.tvTimer.text = "$minute:$seconds"
+    }
+
+    private fun resendOtpToPhoneNumber() {
+        if(isTimerRunning == true){
+            Log.d(TAG,"Timer is Running")
+        }else{
+
+        }
+    }
+
+
 
     private suspend fun signinWithPhoneNumber(credentitals: PhoneAuthCredential) {
         //Signin with phone Number
@@ -109,6 +167,7 @@ class OtpFragment : Fragment(R.layout.fragment_otp) {
                 is NetworkResult.Success -> {
                     withContext(Dispatchers.Main) {
                         findNavController().navigate(R.id.action_otpFragment_to_mainFragment)
+                        countDownTimer.cancel()
                     }
                 }
 
