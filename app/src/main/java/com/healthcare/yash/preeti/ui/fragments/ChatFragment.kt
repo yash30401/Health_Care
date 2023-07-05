46f88540405e4c183cc05c245510b21232095a20
Yashveer singh <yashveersingh30401@gmail.com>
2023-07-05 20:03:30 +0530

    New Bottom Nav Branch Implementation Started

---
+++ app/src/main/java/com/healthcare/yash/preeti/ui/fragments/ChatFragment.kt
@@ -0,0 +1,13 @@
+package com.healthcare.yash.preeti.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.healthcare.yash.preeti.R
+
+class ChatFragment : Fragment(R.layout.fragment_chat) {
+
+
+}
