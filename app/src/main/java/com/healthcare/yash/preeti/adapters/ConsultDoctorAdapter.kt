56fb8e8e7bdf61a8018a9edf13b42077f1bff5be
Yashveer singh <yashveersingh30401@gmail.com>
2023-07-30 22:30:24 +0530

    code - adapter created

--- app/src/main/java/com/healthcare/yash/preeti/adapters/ConsultDoctorAdapter.kt
+++ app/src/main/java/com/healthcare/yash/preeti/adapters/ConsultDoctorAdapter.kt
@@ -3,7 +3,10 @@ package com.healthcare.yash.preeti.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.healthcare.yash.preeti.R
 import com.healthcare.yash.preeti.databinding.DoctorItemLayoutBinding
 import com.healthcare.yash.preeti.models.Doctor
@@ -32,6 +35,14 @@ class ConsultDoctorAdapter : RecyclerView.Adapter<ConsultDoctorAdapter.ConsultDo
         val doctor = doctorsList[position]
 
 
+        Glide.with(holder.itemView).load(doctor.profilePic.toUri()).diskCacheStrategy(
+            DiskCacheStrategy.DATA
+        ).into(holder.binding.ivDoctor)
+
+        holder.binding.tvDoctorName.text = doctor.name
+        holder.binding.tvDoctorBio.text = doctor.about
+        holder.binding.tvDoctorRating.text = doctor.reviewsAndRatings[position].rating.toString()
+        holder.binding.tvDoctorSpecialization.text = doctor.specialization
     }
 
 }
