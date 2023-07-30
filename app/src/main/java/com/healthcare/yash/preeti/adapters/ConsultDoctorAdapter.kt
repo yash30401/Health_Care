fe51e13901be40d58cc587db000f9528bdbef4cf
Yashveer singh <yashveersingh30401@gmail.com>
2023-07-30 22:45:22 +0530

    code - asyncListDiffer Implemented

--- app/src/main/java/com/healthcare/yash/preeti/adapters/ConsultDoctorAdapter.kt
+++ app/src/main/java/com/healthcare/yash/preeti/adapters/ConsultDoctorAdapter.kt
@@ -4,17 +4,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.healthcare.yash.preeti.R
 import com.healthcare.yash.preeti.databinding.DoctorItemLayoutBinding
 import com.healthcare.yash.preeti.models.Doctor
+import com.healthcare.yash.preeti.other.ConsultDoctorDiffUtil
 
 class ConsultDoctorAdapter : RecyclerView.Adapter<ConsultDoctorAdapter.ConsultDoctorViewHolder>() {
 
     private val doctorsList = emptyList<Doctor>()
-
+    private val asyncListDiffer = AsyncListDiffer<Doctor>(this,ConsultDoctorDiffUtil())
     inner class ConsultDoctorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding = DoctorItemLayoutBinding.bind(itemView)
     }
@@ -28,11 +30,11 @@ class ConsultDoctorAdapter : RecyclerView.Adapter<ConsultDoctorAdapter.ConsultDo
     }
 
     override fun getItemCount(): Int {
-        return doctorsList.size
+        return asyncListDiffer.currentList.size
     }
 
     override fun onBindViewHolder(holder: ConsultDoctorViewHolder, position: Int) {
-        val doctor = doctorsList[position]
+        val doctor = asyncListDiffer.currentList[position]
 
 
         Glide.with(holder.itemView).load(doctor.profilePic.toUri()).diskCacheStrategy(
@@ -43,6 +45,10 @@ class ConsultDoctorAdapter : RecyclerView.Adapter<ConsultDoctorAdapter.ConsultDo
         holder.binding.tvDoctorBio.text = doctor.about
         holder.binding.tvDoctorRating.text = doctor.reviewsAndRatings[position].rating.toString()
         holder.binding.tvDoctorSpecialization.text = doctor.specialization
+    }
+
+    fun setData(newList:List<Doctor>){
+        asyncListDiffer.submitList(newList)
     }
 
 }
