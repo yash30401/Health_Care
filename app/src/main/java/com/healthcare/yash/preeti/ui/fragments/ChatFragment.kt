c431d76934d2f21f6bc88a1f602cb54fe96df1b5
Yashveer singh <yashveersingh30401@gmail.com>
2024-01-03 19:25:09 +0530

    lay:- Recent chat item layout created

--- app/src/main/java/com/healthcare/yash/preeti/ui/fragments/ChatFragment.kt
+++ app/src/main/java/com/healthcare/yash/preeti/ui/fragments/ChatFragment.kt
@@ -6,8 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.healthcare.yash.preeti.R
+import com.healthcare.yash.preeti.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment(R.layout.fragment_chat) {
 
+    private var _binding:FragmentChatBinding?=null
+    private val binding get()=_binding!!
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentChatBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
