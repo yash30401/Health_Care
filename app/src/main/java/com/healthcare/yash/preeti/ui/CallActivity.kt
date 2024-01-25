4f1f5345f6eacf7d75533ce3ab5d97bcfd945403
Yashveer singh <yashveersingh30401@gmail.com>
2024-01-25 13:09:20 +0530

    started working on video calling

---
+++ app/src/main/java/com/healthcare/yash/preeti/ui/CallActivity.kt
@@ -0,0 +1,12 @@
+package com.healthcare.yash.preeti.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.healthcare.yash.preeti.R
+
+class CallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call)
+    }
+}
